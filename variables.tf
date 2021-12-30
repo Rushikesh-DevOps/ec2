6fc51bde758889463e9247d31b83025a809f6015
Rushikesh-DevOps <81171429+Rushikesh-DevOps@users.noreply.github.com>
2021-12-30 20:34:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "myJune2021Key"
+  default     =  "Rushikeshdemo"
 }
 
 variable "instance_type" {
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "my-jenkins-security-group"
+  default     = "apacheserver"
 }
 
 variable "tag_name" {
